ad2e5db57961f355e768bf53308f8af3846d09bd
Michel <37207948+roserens@users.noreply.github.com>
2022-04-20 13:33:53 +0200

    added translation of ban recommendations

--- i18n-app.ts
+++ i18n-app.ts
@@ -606,10 +606,15 @@ export const AppTranslations: Translations = {
   },
 
   dotaCoachUsers: {
-    en: "Players using Dota Coach:",
-    de: "Spieler die Dota Coach verwenden:",
-    zh: "此玩家正在使用DotaCoach:",
-    br: "Jogadores usando Dota Coach:",
+    en: "Players using Dota Coach",
+    de: "Spieler die Dota Coach verwenden",
+    fr: "Joueurs utilisant Dota Coach", // #toBeUpdated
+    ru: "Игроки, использующие Dota Coach", // #toBeUpdated
+    id: "Pemain menggunakan Dota Coach", // #toBeUpdated
+    fil: "Ang mga manlalaro gamit ang Dota Coach", // #toBeUpdated
+    zh: "此玩家正在使用DotaCoach",
+    br: "Jogadores usando Dota Coach",
+    es: "Jugadores que usan Dota Coach", // #toBeUpdated
   },
 
   dotaCoachUsersTeam: {
@@ -651,91 +656,246 @@ export const AppTranslations: Translations = {
   playedWithLostWith: {
     en: `<span class="txt-red">Lost</span> as #### with #### as #### #### days ago.`,
     de: `<span class="txt-red">Verloren</span> als #### mit #### als #### vor #### Tagen.`,
+    fr: `<span class="txt-red">Perdu</span> comme #### avec #### comme #### #### il y a quelques jours.`, // #toBeUpdated
+    ru: `<span class="txt-red">Потерян</span> как #### с #### как #### ### дней назад.`, // #toBeUpdated
+    id: `<span class="txt-red">Hilang</span> sebagai ##### dengan ##### #### beberapa hari yang lalu.`, // #toBeUpdated
+    fil: `<span class="txt-red">Nawala</span> bilang #### na may #### bilang ### # # mga araw na nakalipas.`, // #toBeUpdated
+    zh: `几天前以 ##### 的形式@@<span class="txt-red"> 丢失</span>为 ##### ####。`, // #toBeUpdated
+    br: `<span class="txt-red">Perdido</span> como #### com #### como #### #### dias atrás.`, // #toBeUpdated
+    es: `<span class="txt-red">Perdido</span> como #### con #### como #### #### hace días.`, // #toBeUpdated
   },
+
   playedWithWonVs: {
     en: `<span class="txt-green">Won</span> as #### vs #### as #### #### days ago.`,
     de: `<span class="txt-green">Gewonnen</span> als #### gegen #### als #### vor #### Tagen.`,
+    fr: `<span class="txt-green">A gagné</span> #### contre #### comme #### #### il y a quelques jours.`, // #toBeUpdated
+    ru: `<span class="txt-green">Выиграл</span> как #### против #### как #### ### дней назад.`, // #toBeUpdated
+    id: `<span class="txt-green">Menang</span> sebagai ##### vs ######### beberapa hari yang lalu.`, // #toBeUpdated
+    fil: `<span class="txt-green">Nanalo</span> bilang #### vs #### bilang ### # # # mga araw na nakalipas.`, // #toBeUpdated
+    zh: `几天前以 #### vs ##### 作为 #######<span class="txt-green"> 赢</span>了。`, // #toBeUpdated
+    br: `<span class="txt-green">Ganhou</span> como #### vs #### como #### #### dias atrás.`, // #toBeUpdated
+    es: `<span class="txt-green">Ganó</span> #### contra #### como #### #### hace días.`, // #toBeUpdated
   },
   playedWithLostVs: {
     en: `<span class="txt-red">Lost</span> as #### vs #### as #### #### days ago.`,
     de: `<span class="txt-red">Verloren</span> als #### gegen #### als #### vor #### Tagen.`,
+    fr: `<span class="txt-red">Verloren</span> als #### gegen #### als #### vor #### Tagen.`, // #toBeUpdated
+    ru: `<span class="txt-red">Верлорен</span> аль #### геген #### аль ### для #### таген.`, // #toBeUpdated
+    id: `<span class="txt-red">Verloren</span> als ##### # # als #### vor #### Tagen.`, // #toBeUpdated
+    fil: `<span class="txt-red">Verloren</span> als #### berde ### als #### para sa #### Tagen.`, // #toBeUpdated
+    zh: `<span class="txt-red">Verloren</span> als #### gegen #### 也是 #### vor #### Tagen。`, // #toBeUpdated
+    br: `<span class="txt-red">Verloren</span> als #### gegen #### als #### vor #### Tagen.`, // #toBeUpdated
+    es: `<span class="txt-red">Verloren</span> als #### gegen #### als #### vor #### Tagen.`, // #toBeUpdated
   },
 
   enemyHero: {
     en: "Enemy hero",
     de: "Gegner Held",
+    fr: "Héros ennemi", // #toBeUpdated
+    ru: "Вражеский герой", // #toBeUpdated
+    id: "Musuh pahlawan", // #toBeUpdated
+    fil: "Kaaway bayani", // #toBeUpdated
     zh: "敌方英雄",
     br: "Herói inimigo",
+    es: "Héroe enemigo", // #toBeUpdated
   },
 
   inMyLane: {
     en: "My lane",
     de: "Meine Linie",
+    fr: "Ma voie", // #toBeUpdated
+    ru: "Мой переулок", // #toBeUpdated
+    id: "Jalur saya", // #toBeUpdated
+    fil: "Aking lane", // #toBeUpdated
     zh: "和我对线",
     br: "Minha trilha",
+    es: "Mi carril", // #toBeUpdated
   },
 
   counterItemsLane: {
     en: "Counter items to consider (laning)",
     de: "Berücksichtige Konter Gegenstände (Linie)",
+    fr: "Points de comptoir à considérer (atterrissage)", // #toBeUpdated
+    ru: "Предметы счетчика для рассмотрения (laning)", // #toBeUpdated
+    id: "Counter item untuk dipertimbangkan (laning)", // #toBeUpdated
+    fil: "Counter item upang isaalang-alang (laning)", // #toBeUpdated
     zh: "可以考虑针对使用的物品（对线期）",
     br: "Itens counters a se considerar (fase de trilha)",
+    es: "Contraítems a considerar (carriles)", // #toBeUpdated
   },
 
   spectatingMessage: {
     en: "Enjoy watching!",
     de: "Viel Spaß beim Zusehen!",
+    fr: "Profitez de regarder !", // #toBeUpdated
+    ru: "Приятного просмотра!", // #toBeUpdated
+    id: "Nikmati menonton!", // #toBeUpdated
+    fil: "Tangkilikin ang panonood!", // #toBeUpdated
     zh: "享受观看比赛的乐趣吧！",
     br: "Aproveite!",
+    es: "¡Disfruta viendo!", // #toBeUpdated
   },
 
   noRosterMessage: {
     en: "Waiting for information on players...",
     de: "Warte auf Spielerinformationen...",
+    fr: "En attente d'informations sur les joueurs...", // #toBeUpdated
+    ru: "Жду информации об игроках...", // #toBeUpdated
+    id: "Menunggu informasi tentang pemain...", // #toBeUpdated
+    fil: "Naghihintay para sa impormasyon sa mga manlalaro...", // #toBeUpdated
     zh: "正在等待玩家信息",
     br: "Aguardando informação dos jogadores...",
+    es: "Esperando información sobre los jugadores...", // #toBeUpdated
   },
 
   banSuggestions: {
     en: "Ban suggestions",
     de: "Ban Vorschläge",
+    fr: "Suggestions", // #toBeUpdated
+    ru: "Предложения по запрету", // #toBeUpdated
+    id: "Saran larangan", // #toBeUpdated
+    fil: "Mga mungkahi Ban", // #toBeUpdated
     zh: "禁用英雄建议",
     br: "Sugestão de ban",
+    es: "Sugerencias de prohibición", // #toBeUpdated
   },
 
   winLoseStreakNone: {
     en: "No win/lose streak in the last three matches.",
     de: "Keine Sieges/Niederlagen Serie in den letzten 3 Spielen.",
+    fr: "Aucune série de victoires/défaites lors des trois derniers matchs.", // #toBeUpdated
+    ru: "В последних трех матчах нет серии побед и поражений.", // #toBeUpdated
+    id: "Tidak ada kemenangan beruntun dalam tiga pertandingan terakhir.", // #toBeUpdated
+    fil: "Walang manalo/mawalan ng streak sa huling tatlong tugma.", // #toBeUpdated
     zh: "没有三连胜或者三连败",
     br: "Nenhuma sequência de vitória/perda nas últimas 3 partidas",
+    es: "Sin racha de victorias y derrotas en los últimos tres partidos.", // #toBeUpdated
   },
 
   winLoseStreaks: {
     en: "Winning / losing streaks",
     de: "Sieges / Niederlagen Serien",
+    fr: "Conséquences gagnantes/perdantes", // #toBeUpdated
+    ru: "Победные/проигрышные серии", // #toBeUpdated
+    id: "Memenang/kehilangan garis-garis", // #toBeUpdated
+    fil: "Panalong/pagkawala ng streaks", // #toBeUpdated
     zh: "正在连胜或者连败",
     br: "Sequência de vitória/derrota",
+    es: "Rachas ganadoras/perdedoras", // #toBeUpdated
   },
 
   peoplePlayedWith: {
     en: "People you played with",
     de: "Spieler mit denen du gespielt hast",
+    fr: "Les personnes avec qui vous avez joué", // #toBeUpdated
+    ru: "Люди, с которыми вы играли", // #toBeUpdated
+    id: "Orang yang Anda bermain dengan", // #toBeUpdated
+    fil: "Mga taong nilalaro mo", // #toBeUpdated
     zh: "一起玩的队友",
     br: "Pessoas com quem você jogou",
+    es: "Personas con las que jugabas", // #toBeUpdated
   },
 
   heroToBanNone: {
     en: "No spezific hero needs to be banned.",
     de: "Kein spezifischer Held muss gebannt werden.",
+    fr: "Aucun héros spécifique n'a besoin d'être banni.", // #toBeUpdated
+    ru: "Никакого конкретного героя не нужно забанить.", // #toBeUpdated
+    id: "Tidak ada pahlawan spezific perlu dilarang.", // #toBeUpdated
+    fil: "Walang partikular na bayani ang kailangang pinagbawalan.", // #toBeUpdated
     zh: "没有需要去Ban掉的英雄",
     br: "Nenhum herói específico precisa ser banido.",
+    es: "No hay que prohibir a ningún héroe específico.", // #toBeUpdated
   },
 
   heroNotToBanNone: {
-    en: "No spezific hero needs not be banned.",
-    de: "Kein spezifischer Held sollte nicht gebannt werden.",
-    zh: "没有完全不用考虑去Ban的英雄",
-    br: "Nenhum herói específico precisa não ser banido.",
+    en: "All heroes can be banned.",
+    de: "Alle Helden können gebannt werden.",
+    fr: "Tous les héros peuvent être bannis.",
+    ru: "Всех героев можно забанить.", // #toBeUpdated
+    id: "Semua pahlawan bisa dilarang.", // #toBeUpdated
+    fil: "Ang lahat ng mga bayani ay maaaring pinagbawalan.", // #toBeUpdated
+    zh: "所有英雄都可以被封禁。", // #toBeUpdated
+    br: "Todos os heróis podem ser banidos.", // #toBeUpdated
+    es: "Se puede prohibir a todos los héroes.", // #toBeUpdated
+  },
+
+  banMatchCount4You: {
+    en: "You won 4 games in a row",
+    de: "Du hast 4 Spiele in Folge gewonnen",
+    fr: "Vous avez gagné 4 parties d'affilée", // #toBeUpdated
+    ru: "Вы выиграли 4 игры подряд", // #toBeUpdated
+    id: "Anda memenangkan 4 pertandingan berturut-turut", // #toBeUpdated
+    fil: "Nanalo ka ng 4 na laro sa isang hilera", // #toBeUpdated
+    zh: "你连续赢了 4 场比赛", // #toBeUpdated
+    br: "Você venceu 4 jogos seguidos", // #toBeUpdated
+    es: "Has ganado 4 partidos seguidos", // #toBeUpdated
+  },
+  banMatchCount4YourTeam: {
+    en: "#### in your team won 4 games in a row",
+    de: "#### in deiner Mannschaft hat 4 Spiele in Folge gewonnen", // #toBeUpdated
+    fr: "#### de ton équipe a gagné 4 matchs d'affilée", // #toBeUpdated
+    ru: "#### ваша команда выиграла 4 игры подряд", // #toBeUpdated
+    id: "#### di tim Anda memenangkan 4 pertandingan berturut-turut", // #toBeUpdated
+    fil: "#### sa iyong koponan ay nanalo ng 4 na laro sa isang hilera", // #toBeUpdated
+    zh: "#### 在你的队伍中连续赢了 4 场比赛", // #toBeUpdated
+    br: "#### em sua equipe venceu 4 jogos consecutivos", // #toBeUpdated
+    es: "#### en tu equipo ganó 4 partidos seguidos", // #toBeUpdated
+  },
+  banMatchCount4EnemyTeam: {
+    en: "#### in the enemy team won 4 games in a row",
+    de: "#### im gegnerischen Team hat 4 Spiele in Folge gewonnen",
+    fr: "#### dans l'équipe adverse a gagné 4 matchs d'affilée", // #toBeUpdated
+    ru: "#### во вражеской команде победили в 4 играх подряд", // #toBeUpdated
+    id: "#### di tim musuh memenangkan 4 pertandingan berturut-turut", // #toBeUpdated
+    fil: "#### sa koponan ng kaaway ay nanalo ng 4 na laro sa isang hilera", // #toBeUpdated
+    zh: "#### 在敌方队伍中连续赢了 4 场比赛", // #toBeUpdated
+    br: "#### no time inimigo venceu 4 jogos consecutivos", // #toBeUpdated
+    es: "#### en el equipo enemigo ganó 4 partidos seguidos", // #toBeUpdated
+  },
+  banMatchCount5PlusYou: {
+    en: "You have a high win rate of ####% in the last #### matches",
+    de: "Du hast eine hohe Gewinnrate von ####% in den letzten #### Spielen", // #toBeUpdated
+    fr: "Vous avez un taux de victoire élevé de ####% lors des #### derniers matchs", // #toBeUpdated
+    ru: "У вас высокий винрейт #####% в последних матчах ####", // #toBeUpdated
+    id: "Anda memiliki tingkat kemenangan tinggi #####% dalam pertandingan terakhir ####", // #toBeUpdated
+    fil: "Mayroon kang mataas na rate ng panalo ng ####% sa huling #### na mga tugma", // #toBeUpdated
+    zh: "在最后一场 #### 场比赛中，你的胜率高达 ####%", // #toBeUpdated
+    br: "Você tem uma alta taxa de vitórias de ####% nas últimas partidas ####", // #toBeUpdated
+    es: "Tienes una alta tasa de victorias de ####% en los últimos #### partidos", // #toBeUpdated
+  },
+  banMatchCount5PlusYourTeam: {
+    en: "#### in your team has a high win rate of ####% in the last #### matches",
+    de: "#### in deinem Team hat eine hohe Gewinnrate von ####% in den letzten #### Spielen", // #toBeUpdated
+    fr: "#### dans ton équipe a un taux de victoire élevé de ####% lors des #### derniers matchs", // #toBeUpdated
+    ru: "#### в вашей команде высокий винрейт #####% в последних матчах ####", // #toBeUpdated
+    id: "#### di tim Anda memiliki tingkat kemenangan tinggi #####% dalam pertandingan terakhir ####", // #toBeUpdated
+    fil: "#### sa iyong koponan ay may mataas na rate ng panalo ng ####% sa huling #### na mga tugma", // #toBeUpdated
+    zh: "你队伍中的 #### 在过去 #### 场比赛中有 ####% 的高胜率", // #toBeUpdated
+    br: "#### em sua equipe tem uma alta taxa de vitórias de ####% nas últimas #### partidas", // #toBeUpdated
+    es: "#### en tu equipo tiene una alta tasa de victorias de ####% en los últimos #### partidos", // #toBeUpdated
+  },
+  banMatchCount5PlusEnemyTeam: {
+    en: "#### in the enemy team has a high win rate of ####% in the last #### matches",
+    de: "#### im gegnerischen Team hat eine hohe Siegrate von ####% in den letzten #### Spielen", // #toBeUpdated
+    fr: "#### dans l'équipe adverse a un taux de victoire élevé de ####% lors des #### derniers matchs", // #toBeUpdated
+    ru: "#### во вражеской команде высокий винрейт #####% в последних матчах ####", // #toBeUpdated
+    id: "#### di tim musuh memiliki tingkat kemenangan tinggi #####% dalam pertandingan terakhir ####", // #toBeUpdated
+    fil: "#### sa koponan ng kaaway ay may mataas na rate ng panalo ng ####% sa huling #### na mga tugma", // #toBeUpdated
+    zh: "敌方队伍中的 #### 在最后 #### 场比赛中获得 ####% 的高胜率", // #toBeUpdated
+    br: "#### na equipe inimiga tem uma alta taxa de vitória de ####% nas últimas #### partidas", // #toBeUpdated
+    es: "#### en el equipo enemigo tiene una alta tasa de victorias de ####% en los últimos #### partidos", // #toBeUpdated
+  },
+  noBanLowWinRate: {
+    en: "#### in the enemy team has a low win rate and picks hero frequently",
+    de: "#### im gegnerischen Team hat eine niedrige Gewinnrate und wählt häufig Helden", // #toBeUpdated
+    fr: "#### dans l'équipe adverse a un faible taux de victoire et choisit fréquemment son héros", // #toBeUpdated
+    ru: "#### во вражеской команде низкий винрейт и часто выбирает героя", // #toBeUpdated
+    id: "#### di tim musuh memiliki tingkat kemenangan rendah dan sering memilih pahlawan", // #toBeUpdated
+    fil: "#### sa koponan ng kaaway ay may mababang rate ng panalo at madalas na pumili ng bayani", // #toBeUpdated
+    zh: "敌方队伍中的 #### 获胜率较低且经常挑选英雄", // #toBeUpdated
+    br: "#### na equipe inimiga tem uma baixa taxa de vitória e escolhe o herói com frequência", // #toBeUpdated
+    es: "#### en el equipo enemigo tiene una baja tasa de victorias y elige héroe con frecuencia", // #toBeUpdated
   },
 
   playerIsSmurf: {
